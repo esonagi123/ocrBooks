9a1fffff71c0377b1fd0d3ed7b8ef9231e8dabcb
Zoo <qkqrtjdwn0207@gmail.com>
2024-04-02 20:01:19 +0900

    test

---
+++ resources/views/test/zoo_prac.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>prac</title>
+
+    <link rel="stylesheet" href='/css/zoo_prac1.css'>
+</head>
+<body>
+    <div>
+        <form action ="/zoo_prac1" method="POST">
+            <input type="text">
+            <input type="button" value="보내기">
+        </form>
+    </div>
+</body>
+</html>

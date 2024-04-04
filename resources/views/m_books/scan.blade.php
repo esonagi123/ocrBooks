1fecd5d9065b17f801e58c2fab7d1fd9c0ac6da2
KDH <esonagi123@naver.com>
2024-04-04 15:00:40 +0900

    영수증 스캔 페이지

---
+++ resources/views/m_books/scan.blade.php
@@ -0,0 +1,12 @@
+@extends('m_base')
+
+@section('content')
+<div class="billUpload shadow-lg d-flex justify-content-center mt-4">
+    <div class="row align-items-center">
+        <span class="fw-medium text-center text-white fs-5">
+            업로드 &nbsp;<i class="fas fa-cloud-upload-alt"></i>
+        </span>
+    </div> 
+</div>
+
+@endsection()

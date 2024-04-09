ffbb757df897f36774fccddac06ebd86d3703981
KDH <esonagi123@naver.com>
2024-04-09 22:07:12 +0900

    수정

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ Route::get('/m_scan', function () {
     return view('m_books.scan');
 });
 
-<<<<<<< HEAD
+
 // PC
 
 
@@ -29,8 +29,6 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-=======
-<<<<<<< HEAD
 Route::get('/calendar', function () {
     return view('fc.calendar');
 });
@@ -45,8 +43,6 @@ Route::get('/sign_in', function () {
 
 Route::get('/sign_up', function () {
     return view('fc.sign_up');
-=======
->>>>>>> e2786d95c580c304606fb76b7b5d8ce80d753a75
 
 //header
 Route::get('/header', function () {
@@ -64,5 +60,3 @@ Route::get('/login', function () {
 
 Route::get('/base', function () {
     return view('base');
->>>>>>> 248baa5eaac37538ce85e6586f1b1153a6177e5e
-});

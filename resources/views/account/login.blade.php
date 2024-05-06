a762a4f585e6efcbc4b651f0a8d188359bbfd16f
Won <Won@example.com>
2024-05-07 07:48:15 +0900

    회원 관련

--- resources/views/account/login.blade.php
+++ resources/views/account/login.blade.php
@@ -1,11 +1,114 @@
-    <!DOCTYPE html>
-    <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link rel="stylesheet" href="{{ asset('/css/login.css') }}"/>
-        <title></title>
-        <script>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title></title>
+    <link rel="stylesheet" href="{{ asset('/css/login.css') }}">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+    <style>
+        body {
+            background-image: url('/img/isaac-smith-AT77Q0Njnt0-unsplash.jpg');
+            background-size: cover;
+            background-position: center;
+            height: 100vh;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            color: #707070;
+            font-family: 'Arial';
+        }
+
+        .container {
+            background-color: rgba(255, 255, 255, 0.7);
+            border-radius: 10px;
+            box-shadow: 0px 0px 20px rgba(0, 0, 0, 0.5);
+            padding: 40px;
+            max-width: 400px;
+        }
+
+        .container h1 {
+            text-align: center;
+            margin-bottom: 30px;
+            color: #333;
+            font-size: 24px;
+            font-weight: bold;
+        }
+
+        label {
+            color: #707070;
+        }
+
+        input[type="text"],
+        input[type="password"] {
+            padding: 15px;
+            margin-bottom: 20px;
+            border-radius: 5px;
+        }
+
+        .password-input-container {
+            position: relative;
+        }
+
+        #passwordToggleIcon {
+            position: absolute;
+            right: 10px;
+            top: 50%;
+            transform: translateY(-50%);
+            cursor: pointer;
+            color: #888;
+        }
+
+        button[type="submit"] {
+            padding: 10px;
+            border-radius: 50px;
+            font-size: 16px;
+            font-weight: bold;
+            font-family: 'Arial';
+            cursor: pointer;
+        }
+
+        .forgot-password {
+            text-align: center;
+            margin-top: 70px;
+            color: #707070;
+            font-size: 12px;
+        }
+
+        .mb-4 {
+            text-align: center;
+            color: #333;
+            font-size: 12px;
+            margin-bottom: 20px;
+            font-family: 'Arial';
+            font-weight: bold;
+            text-align: left;
+        }
+    </style>
+</head>
+<body>
+    <div class="container">
+    <h1 class="mb-4" style="font-size: rem; font-weight: bold; font-family: 'Palatino'; color: #424042;">OCR 가계부</h1>
+        <form method="POST" action="#" class="mb-4">
+            @csrf
+            <div class="form-group">
+                <label for="id">아이디</label>
+                <input type="text" id="id" name="id" class="form-control">
+            </div>
+            <div class="form-group">
+                <label for="password">비밀번호</label>
+                <div class="password-input-container">
+                    <input type="password" id="password" name="password" class="form-control">
+                    <i class="fas fa-eye-slash" id="passwordToggleIcon" onclick="togglePasswordVisibility()"></i>
+                </div>
+            </div>
+            <button type="submit" class="btn btn-primary btn-block">로그인</button>
+        </form>
+        <p class="forgot-password">아이디 또는 비밀번호를 잊으셨나요? <a href="#" style="color: #007bff;">회원가입</a></p>
+    </div>
+
+    <script>
         function togglePasswordVisibility() {
             var passwordInput = document.getElementById("password");
             var icon = document.getElementById("passwordToggleIcon");
@@ -20,42 +123,5 @@
             }
         }
     </script>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
-    <style>
-        .container {
-            margin: 0 auto;
-            padding: 70px;
-        }
-
-        .password-input-container {
-            position: relative;
-        }
-
-        #passwordToggleIcon {
-            position: absolute;
-            right: 5px;
-            top: 50%;
-            transform: translateY(-50%);
-            cursor: pointer;
-        }
-    </style>
-    </head>
-    <body>
-        <div class="container">
-            <h1>OCR 가계부 로그인</h1>
-            <form method="POST" action="#">
-                @csrf
-                <label for="id">ID</label>
-                <input type="text" id="id" name="id">
-                <label for="password">Password</label>
-                <div class="password-input-container">
-                <input type="password" id="password" name="password">
-                <i class="fas fa-eye-slash" id="passwordToggleIcon" onclick="togglePasswordVisibility()"></i>
-                </div>
-                <button type="submit">로그인</button>
-                <button type="submit">회원가입</button>
-            <h5>아이디 또는 비밀번호를 잊으셨나요?</h5>
-            </form>
-        </div>
-    </body>
-    </html>
+</body>
+</html>
--- resources/views/account/signup.blade.php
+++ resources/views/account/signup.blade.php
@@ -3,28 +3,115 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="{{ asset('/css/signup.css') }}"/>
     <title></title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+    <style>
+        body {
+            background-image: url('/img/isaac-smith-AT77Q0Njnt0-unsplash.jpg');
+            background-size: cover;
+            background-position: center;
+            height: 100vh;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            color: #707070;
+            font-family: 'Arial';
+        }
+
+        .container {
+            background-color: rgba(255, 255, 255, 0.7);
+            border-radius: 10px;
+            box-shadow: 0px 0px 20px rgba(0, 0, 0, 0.5);
+            padding: 20px;
+            max-width: 400px;
+            font-size: 16px;
+            font-weight: bold;
+        }
+
+        .container h2 {
+            text-align: center;
+            margin-bottom: 30px;
+            color: #424042;
+            font-size: 24px;
+            font-weight: bold;
+        }
+
+        label {
+            color: #707070;
+        }
+
+        input[type="text"],
+        input[type="password"] {
+            padding: 15px;
+            margin-bottom: 20px;
+            border-radius: 5px;
+        }
+        
+        .form-control-file {
+            color: #707070;
+            font-weight: bold;
+        }
+
+        .custom-file-label::after {
+            color: #707070;
+        }
+
+        button[type="submit"] {
+            padding: 10px;
+            border-radius: 50px;
+            font-size: 16px;
+            font-weight: bold;
+            font-family: 'Arial';
+            cursor: pointer;
+            background-color: #007bff;
+            color: #fff;
+            border: none;
+            width: 100%;
+        }
+
+        .mb-4 {
+            text-align: center;
+            color: #333;
+            font-size: 12px;
+            margin-bottom: 20px;
+            font-family: 'Arial';
+            font-weight: bold;
+            text-align: left;
+        }
+    </style>
 </head>
 <body>
     <div class="container">
         <h2>회원 가입</h2>
-        <form method="POST" action="#">
+        <form method="POST" action="#" class="mb-4">
             @csrf
-            <label for="username">사용자 이름</label>
-            <input type="text" id="username" name="username" required>
-
-            <label for="phonenumber">전화번호</label>
-            <input type="phonenumber" id="phonenumber" name="phonenumber" required>
-
-            <label for="password">비밀번호</label>
-            <input type="password" id="password" name="password" required>
-
-            <label for="confirm_password">비밀번호 확인</label>
-            <input type="password" id="confirm_password" name="confirm_password" required>
+            <div class="form-group">
+                <label for="name">이름</label>
+                <input type="text" id="name" name="name" class="form-control">
+            </div>
+            <div class="form-group">
+                <label for="birth_date">생년월일</label>
+                <input type="date" id="birth_date" name="birth_date" class="form-control">
+            </div>
+            <div class="form-group">
+                <label for="ID">아이디</label>
+                <input type="text" id="id" name="id" class="form-control">
+            </div>
+            <div class="form-group">
+                <label for="new_password">새로운 비밀번호</label>
+                <input type="password" id="new_password" name="new_password" class="form-control">
+            </div>
+            <div class="form-group">
+                <label for="confirm_password">새로운 비밀번호 확인</label>
+                <input type="password" id="confirm_password" name="confirm_password" class="form-control">
+            </div>
+            <div class="form-group">
+                <label for="profile_image">프로필 이미지</label>
+                <input type="file" id="profile_image" name="profile_image" class="form-control-file">
+            </div>
 
             <button type="submit">가입하기</button>
-            
         </form>
     </div>
 </body>

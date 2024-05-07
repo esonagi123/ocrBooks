44863b2a504aada83bbe375210d0ca8d18c940da
KDH <esonagi123@naver.com>
2024-05-07 19:41:58 +0900

    0507 KDH

--- routes/web.php
+++ routes/web.php
@@ -2,17 +2,28 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ocrAPI;
+use App\Http\Controllers\Account\Join;
+use App\Http\Controllers\Account\Login;
+
+
+Route::get('/newBase', function () {
+    return view('newBase');
+});
 
 
 // 모바일
 
+// 메인 화면
 Route::get('/m_main', function () {
     return view('m_main.index');
 });
 
+// 영수증 업로드
 Route::get('/m_scan', function () {
     return view('m_books.scan');
 });
+
+// OCR API
 Route::post('api/requestOCR', [ocrAPI::class, 'upload'])->name('upload');
 
 Route::get('/', function () {
@@ -34,6 +45,32 @@ Route::get('/sign_in', function () {
 Route::get('/sign_up', function () {
     return view('m_sign.sign_up');
 });
+
+// 로그인
+Route::get('/m_login', function () {
+    return view('m_account.login');
+});
+Route::get('login', [Login::class, 'index'])->name('login');
+
+// 회원가입
+Route::get('/m_join', function () {
+    return view('m_account.join');
+});
+Route::get('register', [Join::class, 'index'])->name('register');
+Route::post('register/join', [Join::class, 'store'])->name('register.join');
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 // PC

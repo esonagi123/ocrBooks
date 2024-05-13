2532f2f226b3b78082523879adad1170c907df88
Won <Won@example.com>
2024-05-14 02:34:35 +0900

    모바일화

--- resources/views/account/edit.blade.php
+++ resources/views/account/edit.blade.php
@@ -1,127 +1,51 @@
 <!DOCTYPE html>
-<html lang="en">
+<html>
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title></title>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
-    <style>
-        body {
-            background-image: url('/img/isaac-smith-AT77Q0Njnt0-unsplash.jpg');
-            background-size: cover;
-            background-position: center;
-            height: 100vh;
-            display: flex;
-            justify-content: center;
-            align-items: center;
-            color: #707070;
-            font-family: 'Arial';
-        }
+    <!-- 부트스트랩 CDN -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+    <!-- FontAwesome -->
+    <script src="https://kit.fontawesome.com/649102945e.js" crossorigin="anonymous"></script>
+    <!-- ionicons -->
+    <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
+    <script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
+    <!-- CSS 로드 -->
+    <link rel="stylesheet" href="{{ asset('css/core.css') }}">
+    <link rel="stylesheet" href="{{ asset('/assets/vendor/css/core.css') }}" class="template-customizer-core-css" />
+    <link rel="stylesheet" href="{{ asset('/assets/vendor/css/theme-default.css') }}" class="template-customizer-theme-css" />
+    <!-- Vendors CSS -->
+    <link rel="stylesheet" href="{{ asset('/assets/vendor/libs/perfect-scrollbar/perfect-scrollbar.css') }}" />
+    <link rel="stylesheet" href="{{ asset('/assets/vendor/libs/apex-charts/apex-charts.css') }}" />
 
-        .container {
-            background-color: rgba(255, 255, 255, 0.7);
-            border-radius: 10px;
-            box-shadow: 0px 0px 20px rgba(0, 0, 0, 0.5);
-            padding: 40px;
-            max-width: 400px;
-            font-size: 16px;
-            font-weight: bold;
-        }
-
-        .container h1 {
-            text-align: center;
-            margin-bottom: 30px;
-            color: #424042;
-            font-size: 24px;
-            font-weight: bold;
-        }
-
-        label {
-            color: #707070;
-        }
-
-        input[type="text"],
-        input[type="password"] {
-            padding: 15px;
-            margin-bottom: 20px;
-            border-radius: 5px;
-        }
-
-        .password-input-container {
-            position: relative;
-        }
-
-        #passwordToggleIcon {
-            position: absolute;
-            right: 10px;
-            top: 50%;
-            transform: translateY(-50%);
-            cursor: pointer;
-            color: #888;
-        }
-
-        button[type="submit"] {
-            padding: 10px;
-            border-radius: 50px;
-            font-size: 16px;
-            font-weight: bold;
-            font-family: 'Arial';
-            cursor: pointer;
-        }
-
-        .forgot-password {
-            text-align: center;
-            margin-top: 70px;
-            color: #707070;
-            font-size: 12px;
-        }
-
-        .mb-4 {
-            text-align: center;
-            color: #333;
-            font-size: 12px;
-            margin-bottom: 20px;
-            font-family: 'Arial';
-            font-weight: bold;
-            text-align: left;
-        }
-
-        .card {
-            margin-top: 5rem;
-        }
-
-        .card-header {
-            background-color: rgba(0, 0, 0, 0.8);
-            color: #fff;
-            text-align: center;
-            font-weight: bold;
-            font-size: 2rem;
-        }
-
-        .form-group label {
-            font-weight: bold;
-            color: #707070;
-        }
-
-        .btn {
-            font-weight: bold;
-        }
-
-        
-
-        input[type="date"]::-webkit-inner-spin-button,
-        input[type="date"]::-webkit-calendar-picker-indicator {
-            color: #707070;
-            font-weight: bold;
-            font-size: 20px;
-        }
-
-    </style>
+    <link rel="stylesheet" href="{{ asset('css/edit.css') }}">
 </head>
 <body>
+<div class="ms-4 me-4">
+      <div class="container">
+          <div class="row mt-4 text-center align-items-center" >
+              <div class="col-8">
+                  <div class="row textGrey fw-medium">
+                    환영합니다!
+                  </div>
+                  <div class="row fw-bold">
+                    --- 님
+                  </div>
+              </div>
+              <div class="col text-end">
+                  <div class="dropdown">
+                    <img class="avatar" src="{{ asset('img/avatar0.png') }}" alt="프로필" data-bs-toggle="dropdown" aria-expanded="false">
+                    <ul class="dropdown-menu">
+                      <li><a class="dropdown-item" href="#">마이페이지</a></li>
+                      <li><a class="dropdown-item" href="{{ url('m_login') }}">로그인</a></li>
+                    </ul>                  
+                  </div>
+                </div>
+          </div>
+      </div>
     <div class="container">
-        <h1 class="mb-4">OCR 가계부</h1>
+        <h1 class="mb-4">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;회원 정보 수정</h1>
         <form method="POST" action="#" class="mb-4">
             @csrf
             <div class="form-group">
@@ -136,16 +60,47 @@
                 <label for="ID">아이디</label>
                 <input type="text" id="id" name="id" class="form-control">
             </div>
-            <div class="form-group">
+            <div class="form-group password-input-container">
                 <label for="new_password">새로운 비밀번호</label>
                 <input type="password" id="new_password" name="new_password" class="form-control">
+                <i class="fas fa-eye-slash" id="passwordToggleIcon" onclick="togglePasswordVisibility()"></i>
             </div>
-            <div class="form-group">
+            <div class="form-group password-input-container">
                 <label for="confirm_password">새로운 비밀번호 확인</label>
                 <input type="password" id="confirm_password" name="confirm_password" class="form-control">
+                <i class="fas fa-eye-slash" id="confirmPasswordToggleIcon" onclick="toggleConfirmPasswordVisibility()"></i>
             </div>
-            <button type="submit" class="btn btn-primary btn-block">저장</button>
+            <button type="submit" class="btn btn-primary btn-block">수정</button>
         </form>
     </div>
+    <script>
+        function togglePasswordVisibility() {
+            var passwordInput = document.getElementById("new_password");
+            var icon = document.getElementById("passwordToggleIcon");
+            if (passwordInput.type === "password") {
+                passwordInput.type = "text";
+                icon.classList.remove("fa-eye-slash");
+                icon.classList.add("fa-eye");
+            } else {
+                passwordInput.type = "password";
+                icon.classList.remove("fa-eye");
+                icon.classList.add("fa-eye-slash");
+            }
+        }
+
+        function toggleConfirmPasswordVisibility() {
+            var confirmPasswordInput = document.getElementById("confirm_password");
+            var icon = document.getElementById("confirmPasswordToggleIcon");
+            if (confirmPasswordInput.type === "password") {
+                confirmPasswordInput.type = "text";
+                icon.classList.remove("fa-eye-slash");
+                icon.classList.add("fa-eye");
+            } else {
+                confirmPasswordInput.type = "password";
+                icon.classList.remove("fa-eye");
+                icon.classList.add("fa-eye-slash");
+            }
+        }
+    </script>
 </body>
 </html>

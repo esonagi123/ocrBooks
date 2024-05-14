6079e2b825fc108b6b1db029682107cb080b4384
Zoo <qkqrtjdwn0207@gmail.com>
2024-05-14 19:15:55 +0900

    내역화면+css

--- routes/web.php
+++ routes/web.php
@@ -13,4 +13,9 @@ Route::get('/scan', function () {
     return view('books.scan');
 });
 
+Route::get('/uselist', function () { 
+    return view('books.uselist');
+});
+
+
 Route::post('api/requestOCR', [ocrAPI::class, 'upload'])->name('upload');
